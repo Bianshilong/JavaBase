36ca60b1b56a2cdff51ac0b775449a1c05a267d3
Bianshilong <442668830@qq.com>
2017-09-03 21:24:40 +0800

    this is my first commit!
    Signed-off-by: Bianshilong <442668830@qq.com>

---
+++ JavaBase/src/com/bsl/javacore/datatype/NameAge.java
@@ -0,0 +1,8 @@
+package com.bsl.javacore.datatype;
+
+public class NameAge {
+	public static void main(String[] args){
+		System.out.println("您的名字是：" + args[0]);
+		System.out.println("您的年龄是：" + args[1]);
+	}
+}
---
+++ JavaBase/src/com/bsl/javacore/datatype/TestType.java
@@ -0,0 +1,14 @@
+package com.bsl.javacore.datatype;
+
+public class TestType {
+	public static void main(String[] args){
+		double score = 98.55;
+		String name = "三胖";
+		char sex = '男';
+		
+		System.out.println("本次考试最高分：" + score);
+		System.out.println("最高分得主姓名：" + name);
+		System.out.println("最高分得主性别：" + sex);
+	}
+
+}
