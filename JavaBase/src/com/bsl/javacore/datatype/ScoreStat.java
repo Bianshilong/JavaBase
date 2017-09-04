a1021fc67ea653a02c4d2bef25c5f8821d8fd565
Bianshilong <442668830@qq.com>
2017-09-04 21:05:51 +0800

    Signed-off-by: Bianshilong <442668830@qq.com>

---
+++ JavaBase/src/com/bsl/javacore/datatype/ScoreStat.java
@@ -0,0 +1,10 @@
+package com.bsl.javacore.datatype;
+import java.util.Scanner;
+public class ScoreStat {
+	public static void main(){
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("STB的成绩是：");
+		int stb = input.nextInt();	
+	}
+}
