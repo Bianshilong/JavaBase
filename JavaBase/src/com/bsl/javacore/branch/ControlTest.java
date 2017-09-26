13830da1c3947c93630c583a6bb0a495c5e3140e
Bianshilong <442668830@qq.com>
2017-09-26 15:25:28 +0800

    test

--- JavaBase/src/com/bsl/javacore/branch/ControlTest.java
+++ JavaBase/src/com/bsl/javacore/branch/ControlTest.java
@@ -26,6 +26,8 @@ public class ControlTest {
 			}
 			System.out.println();
 		}
+	
+	
 	}
 
 }
---
+++ JavaBase/src/com/bsl/javacore/test/Test1.java
@@ -0,0 +1,16 @@
+package com.bsl.javacore.test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		Double i1 = 100.0;
+		Double i2 = 100.0;
+		Double i3 = 200.0;
+		Double i4 = 200.0;
+
+		System.out.println(i1 == i2);
+		System.out.println(i3 == i4);
+	}
+
+}
