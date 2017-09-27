b1f43e6b63ddc5823253be8960c083238def5a87
Bianshilong <442668830@qq.com>
2017-09-27 18:24:00 +0800

    add for test

--- JavaBase/src/com/bsl/javacore/test/Test3.java
+++ JavaBase/src/com/bsl/javacore/test/Test3.java
@@ -1,5 +1,6 @@
 package com.bsl.javacore.test;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import org.apache.log4j.Logger;
@@ -19,17 +20,15 @@ public class Test3 {
 		b=input.nextInt();
 		try {
 			int c=a/b;
-			if (b==0) {
-				b=input.nextInt();
-			}
+			logger.info("输入数据正常");
 		} catch (ArithmeticException e) {
 			logger.warn("除数不能为零");
-		}catch (IllegalArgumentException e) {
+		}catch (InputMismatchException e) {
 			logger.error("被除数和除数必须是整数！");
-		}catch(Exception e){
-			logger.info("输入数据正常！");
 		}
-		
+		finally {
+			input.next();
+		}
 	}
 		
 	
